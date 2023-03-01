f89a6e0ab1f76a9f0df89bb74067937cbb5b0213
lairaalmas <lairaalmas@gmail.com>
2023-02-28 23:37:59 -0300

    remove unecessary code and install sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const prefix = require("gulp-autoprefixer");
+const minify = require("gulp-clean-css");
+const rename = require("gulp-rename");
+
+// .scss -> .min.css
+gulp.task("sass", function () {
+  return gulp
+    .src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(minify())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest("./src/css"));
+});
+
+// watch scss files
+gulp.task(
+  "watch",
+  gulp.series("sass", function () {
+    gulp.watch("src/scss/*.scss", gulp.series("sass"));
+  })
+);
+
+gulp.task("start", gulp.series("watch"));
