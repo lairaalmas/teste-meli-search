53e6112a01d72e23e490a6f490f07cf251739262
lairaalmas <lairaalmas@gmail.com>
2023-03-04 21:56:18 -0300

    create a search layout isolated from header and dummy breadcrumb component

---
+++ src/components/Breadcrumb/index.js
@@ -0,0 +1,5 @@
+const Breadcrumb = (props) => {
+  return <h1>Breadcrumb component</h1>;
+};
+
+export default Breadcrumb;
--- src/pages/SearchLayout/index.js
+++ src/pages/SearchLayout/index.js
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Breadcrumb from "../../components/Breadcrumb";
 
 const SearchLayout = () => {
   return (
     <>
+      <Breadcrumb />
       <Outlet />
     </>
   );
