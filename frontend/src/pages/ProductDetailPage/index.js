4e34b66d9aff8b18d6934570ef3ca06c9f6065c2
lairaalmas <lairaalmas@gmail.com>
2023-03-08 17:08:03 -0300

    refactor product detail page

--- frontend/src/pages/ProductDetailPage/index.js
+++ frontend/src/pages/ProductDetailPage/index.js
@@ -7,53 +7,59 @@ import "../../css/ProductDetail.min.css";
 
 const ProductDetailPage = () => {
   const data = useLoaderData();
-  let item, amount;
 
-  if (data && data.item) {
-    item = data.item;
+  let content;
 
-    amount = Intl.NumberFormat("es-AR", {
+  if (!data || !data.item) {
+    content = <p>No se encontraron detalles del productos</p>;
+  } else {
+    const item = data.item;
+
+    const amount = Intl.NumberFormat("es-AR", {
       style: "currency",
       currency: item.price.currency,
     }).format(item.price.amount);
-    amount = amount.split(",")[0];
-  } else {
-    return (
-      <Card className="ProductDetail">
-        <h2>Erro</h2>
-      </Card>
+
+    content = (
+      <>
+        <div className="ProductDetail__content">
+          <div className="ProductDetail__content__picture">
+            <img src={item.picture} alt={item.picture} />
+          </div>
+
+          <div className="ProductDetail__content__description">
+            <h2>Descripción del producto</h2>
+
+            <p>{item.description}</p>
+          </div>
+        </div>
+
+        <div className="ProductDetail__information">
+          <div className="ProductDetail__information__extra">
+            <small>
+              {item.condition} - {item.sold_quantity} vendidos
+            </small>
+          </div>
+
+          <h1 className="ProductDetail__information__title">{item.title}</h1>
+
+          <div className="ProductDetail__information__price">
+            <strong>
+              <span>{amount.split(",")[0]}</span>
+              <span>{item.price.decimals}</span>
+            </strong>
+          </div>
+
+          <button type="button">Comprar</button>
+        </div>
+      </>
     );
   }
 
   return (
     <>
-      <Breadcrumb categories={[]} />
-      <Card className="ProductDetail">
-        <div className="ProductDetail__main">
-          <div className="ProductDetail__main__img">
-            <img src={item.picture} alt={item.picture} />
-          </div>
-          <div className="ProductDetail__main__info">
-            <p className="ProductDetail__main__info__small">
-              {item.condition} - {item.sold_quantity} vendidos
-            </p>
-            <h2 className="ProductDetail__main__info__title">{item.title}</h2>
-            <div className="ProductDetail__main__info__price">
-              <span>{amount}</span>
-              <span>{item.price.decimals}</span>
-            </div>
-            <button>Comprar</button>
-          </div>
-        </div>
-        <div className="ProductDetail__description">
-          <h3 className="ProductDetail__description__title">
-            Descripción del producto
-          </h3>
-          <p className="ProductDetail__description__content">
-            {item.description}
-          </p>
-        </div>
-      </Card>
+      <Breadcrumb />
+      <Card className="ProductDetail">{content}</Card>
     </>
   );
 };
